2a4188f4219e644dfb0697132d85ea574a1a7ba4
Steeven Lim <steeven_lim@yahoo.com>
2021-07-25 22:53:31 +0700

    add site config

--- app/Http/Controllers/Site/Controller.php
+++ app/Http/Controllers/Site/Controller.php
@@ -14,7 +14,7 @@ class Controller extends \App\Http\Controllers\Controller
 {
     public function __construct()
     {
-        $theme = Config::get('admin.theme');
+        $theme = Config::get('site.theme');
 
         View::addLocation(base_path() . '/resources/themes/site/' . $theme . '/layouts/');
         View::addLocation(base_path() . '/resources/themes/site/' . $theme . '/views/');
